896a68037291443c08e1825879520b981fe523d9
Timofey Kremko <timkremko@gmail.com>
2022-11-01 17:39:10 +0100

    render: list of users

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,14 +4,15 @@ import { useSearchUsersQuery } from "../store/github/github.api";
 
 export default function HomePage() {
   const [search, setSearch] = useState("");
+  const [dropdown, setDropdown] = useState(false);
   const debounced =  useDebounce(search)
   const { isError, isLoading, data } = useSearchUsersQuery(debounced, {
     skip: debounced.length < 3,
   });
 
   useEffect(() => {
-    console.log(search);
-  }, [debounced]);
+    setDropdown(debounced.length > 3 && data?.length! > 0)
+  }, [debounced, data]);
 
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
@@ -27,9 +28,19 @@ export default function HomePage() {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
-          { isLoading && <p className="text-center">Loading...</p> }
-        </ul>
+        {dropdown && (
+          <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white overflow-y-scroll">
+            {isLoading && <p className="text-center">Loading...</p>}
+            {data?.map((user) => (
+              <li
+                key={user.id}
+                className="py-2 px-4  hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+              >
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
